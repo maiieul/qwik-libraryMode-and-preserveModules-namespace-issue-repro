5e5bce8ea8424816a0d6db1cb83cd08e22a9bf0b
maieulchevalier <maieul.chevalier@gmail.com>
2023-11-22 15:08:51 +0100

    make it more minimal

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,7 @@ import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
-  let rollupOptions = {};
-
-  if (process.env.npm_lifecycle_event === "build.client") {
-    // Client-specific configuration
-    rollupOptions = {
-      output: {
-        // Customize the client build structure
-        entryFileNames: (fileInfo:any) => {
-          console.log("fileInfo", fileInfo);
-          return `build/[name]-[hash].js`
-        },
-        chunkFileNames: (fileInfo:any) => {
-          console.log("fileInfo", fileInfo);
-          return `build/[name]-[hash].js`
-        },
-        assetFileNames: `build/[name]-[hash].[ext]`,
-      },
-    };
-  }
-
   return {
-    build: {
-      rollupOptions,
-    },
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
